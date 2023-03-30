2fb451cc31dc1541841d8314232396ff0f393906
Charelle Jazmin <charellejazmin@gmail.com>
2023-03-30 18:46:10 -0400

    init commit

--- main.js
+++ main.js
@@ -1,73 +1,85 @@
-//
-//  JS File
-//  You may remove the code below - it's just boilerplate
-//
+// arrays of keys by row
+const rowF = ['F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12','Escape'];
+const row0 = ['Digit1','Digit2','Digit3','Digit4','Digit5','Digit6','Digit7','Digit8','Digit9','Digit0','Minus','Equal','Backquote','Backspace'];
+const row1 = ['KeyQ','KeyW','KeyE','KeyR','KeyT','KeyY','KeyU','KeyI','KeyO','KeyP','BracketLeft','BracketRight','Backslash','Tab'];
+const row2 = ['KeyA','KeyS','KeyD','KeyF','KeyG','KeyH','KeyJ','KeyK','KeyL','Semicolon','Enter','Quote','CapsLock'];
+const row3 = ['KeyZ','KeyX','KeyC','KeyV','KeyB','KeyN','KeyM','Comma','Period','Slash','ShiftLeft','ShiftRight'];
+const otherKeys = ['ControlLeft','AltLeft','MetaLeft','MetaRight','AltRight','ArrowUp','ArrowDown','ArrowLeft','ArrowRight'];
 
-//
-// Variables
-//
 
-// Constants
-const appID = "app";
-const headingText = "Develop. Preview. Ship.";
-const headingTextIcon = "🚀";
-const projectDueDate = "11 April 2023 11:59";
+// audio elements on pressed 
+const row1KD = new Audio("https://cdn.glitch.global/14f6277a-4737-44bb-9f79-8f0fc9ce983d/src_assets_audio_topre_press_GENERIC_R1.mp3?v=1680030921174");
+const row2KD = new Audio("https://cdn.glitch.global/14f6277a-4737-44bb-9f79-8f0fc9ce983d/src_assets_audio_topre_press_GENERIC_R2.mp3?v=1680032471540");
+const row3KD = new Audio("https://cdn.glitch.global/14f6277a-4737-44bb-9f79-8f0fc9ce983d/src_assets_audio_topre_press_GENERIC_R3.mp3?v=1680032469951");
+const spaceKD = new Audio("https://cdn.glitch.global/14f6277a-4737-44bb-9f79-8f0fc9ce983d/src_assets_audio_topre_press_SPACE.mp3?v=1680031642388");
 
-// Variables
-let countdownDate = new Date(projectDueDate);
+// audio elements on release
+const rowKU = new Audio("https://cdn.glitch.global/14f6277a-4737-44bb-9f79-8f0fc9ce983d/src_assets_audio_topre_release_GENERIC.mp3?v=1680030926231");
 
-// DOM Elements
-let appContainer = document.getElementById(appID);
+const spaceKU = new Audio("https://cdn.glitch.global/14f6277a-4737-44bb-9f79-8f0fc9ce983d/src_assets_audio_topre_release_SPACE.mp3?v=1680031644194");
 
-//
-// Functions
-//
-
-function calculateDaysLeft(countdownDate) {
-  const now = new Date().getTime();
-  const countdown = new Date(countdownDate).getTime();
-
-  const difference = (countdown - now) / 1000;
-
-  // Countdown passed already
-  if (difference < 1) {
-    return null;
+// event listener to the document to detect when a key is pressed
+document.addEventListener("keydown", function(event) {
+  // Play the audio file if the "a" key is pressed
+  if (rowF.includes(event.code)) {
+    row1KD.currentTime = 0;
+    row1KD.play();
   }
-
-  const days = Math.floor(difference / (60 * 60 * 24));
-
-  return days;
-}
-
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
+    else if (row0.includes(event.code)) {
+    row1KD.currentTime = 0;
+    row1KD.play();
   }
-
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  const daysLeft = calculateDaysLeft(countdownDate);
-  let headingTextCalculated = headingText;
-
-  if (daysLeft) {
-    headingTextCalculated = headingTextCalculated.concat(
-      " In ",
-      daysLeft.toString(),
-      " days "
-    );
+    else if (row1.includes(event.code)) {
+    row1KD.currentTime = 0;
+    row1KD.play();
   }
-  h1.textContent = headingTextCalculated.concat(headingTextIcon);
-  appContainer.appendChild(h1);
+      else if (row2.includes(event.code)) {
+      row2KD.currentTime = 0;
+      row2KD.play();
+  }
+      else if (row3.includes(event.code)) {
+      row3.currentTime = 0;
+      row3KD.play();
+  }
+      else if (otherKeys.includes(event.code)) {
+      row3.currentTime = 0;
+      row1KD.play();
+  }
+    else if (event.code === 'Space') {
+      spaceKD.currentTime = 0;
+      spaceKD.play();
+  }
+  
+});
 
-  // Init complete
-  console.log("App successfully initialised");
-}
+// event listener to the document to detect when a key is pressed
+document.addEventListener("keyup", function(event) {
+  // Play the audio file if the "a" key is pressed
+  if (rowF.includes(event.code)||row0.includes(event.code)||row1.includes(event.code)||row2.includes(event.code)||row3.includes(event.code)||otherKeys.includes(event.code)) {
+    rowKU.currentTime = 0;
+    rowKU.play();
+  }
+    else if (event.code === 'Space') {
+      spaceKU.currentTime = 0;
+      spaceKU.play();
+  }
+  
+  
+});
 
-//
-// Inits & Event Listeners
-//
+// get the textbox element
+const textbox = document.querySelector('.textbox');
+const clearBtn = document.querySelector('.clearBtn');
 
-inititialise();
+// listen for input events on the textbox
+textbox.addEventListener('input', function() {
+  // get the text content of the textbox
+  const text = textbox.textContent;
+
+  console.log(text);
+});
+
+
+clearBtn.addEventListener('click', function() {
+  textbox.textContent = '';
+});
