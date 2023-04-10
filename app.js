790539b6b5e7ff637a233933208c629a1b87fadb
Tarek Sallam <tareksallam@cmail.carleton.ca>
2023-04-09 21:43:43 -0400

    Added a link gen function, database connection

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const KeyData = require('./Data.js');
-const PORT = 3000;
+const storeJSON = require('./extraJS/post.js');
 
+const PORT = 3000;
 const app = express();
 
-//mongoose.connect('mongodb://localhost:27017');
+// CONNECT TO DATABASE
+mongoose.connect('mongodb://localhost:27017/KeyboardDB', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('Error connecting to MongoDB', err));
 
+// SEND DATA TO CLIENT
+app.use(express.static('static'));
 app.use(express.static('public'));
 
+// PARSE INCOMING JSON
 app.use(express.json());
 
-app.post('/submitdata', (req, res) => {
-    const data = req.body;
-    console.log(data);
-    res.send('JSON data received and stored successfully!');
-});
+// STORE INCOMING JSON IN DATABASE (middleware that imports post.js function storeJSON)
+app.use(storeJSON);
 
+// SERVER LISTEN
 app.listen(PORT, () => {
     console.log('Server started on port:' + PORT);
 });
---
+++ extraJS/data.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// DEFINE THE SCHEMA
+const Schema = new mongoose.Schema({
+    data: Object,
+    link: String
+});
+  
+// DEFINE THE MODEL AND COLLECTION NAME
+const model = mongoose.model('KeyboardJSON', Schema);
+
+// EXPOR THE MODEL
+module.exports = model;
